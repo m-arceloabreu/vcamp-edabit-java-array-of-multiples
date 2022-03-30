5f133d6df706250ea4312210b745f4a97056d90e
Marcelo Abreu <87956812+m-arceloabreu@users.noreply.github.com>
2022-03-30 20:58:32 -0300

    Inserção de Comentarios

--- Challenge.java
+++ Challenge.java
@@ -4,11 +4,13 @@ package challenge.challenge;
 
 public class Challenge {
 	public static void main(String[] args) {
+		// Declarando variaveis
+		int num = 7;	// Será o valor do index 0 da minha array
+		int length = 5; // Será o numero de indexs da minha array
 		
-		int num = 7;
-		int length = 5;
 		
-		/* ---Option if you want to type your own values  ---
+		/* 
+---Caso queira inserir outros valores para Num e Length  utilize o codigo abaixo  ---
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Type a value: ");
 		int num = sc.nextInt();
@@ -17,15 +19,17 @@ public class Challenge {
 		
 		*/
 		
+		// Puxando o método arrayOfMultiples que vai gerar meu código
 		arrayOfMultiples(num, length);
 		
 		
 	}
 	
+	//Método utilizado para realizar o desafio
 	public static int[] arrayOfMultiples(int num, int length) {
 			
-			int[] list = new int[length];
-			for(int i=0; i<list.length ;i++)
+			int[] list = new int[length];	// Criação da array com length como tamanho
+			for(int i=0; i<list.length ;i++) //Laço de repetição que ocorrera até minha array ter Length de tamanho
 		    {
 				list[i] = ((i + 1)* num);
 		        System.out.print(list[i] + " ");
@@ -33,7 +37,5 @@ public class Challenge {
 		return list;
 	}
 }
-
-
 
 
